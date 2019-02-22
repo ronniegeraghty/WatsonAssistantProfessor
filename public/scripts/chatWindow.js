cac04ede515df51e49d64f0d944f7798da01b898
Ronald Geraghty <ronniegerag@gmail.com>
2019-02-22 10:33:21 -0500

    added chatBtn and chatbox appears and disappears

--- public/scripts/chatWindow.js
+++ public/scripts/chatWindow.js
@@ -1,11 +1,25 @@
-const Talk = require('talkjs');
+$(document).ready(function () {
 
-var sess = new Talk.Session ({
-    id: "tFSK3c9y",
-    name: "Ronald",
-    email: "ronald.geraghty@ibm.com",
-    photoUrl: "https://demo.talkjs.com/img/sebastian.jpg",
-    welcomeMessage: "Hey, how can I help?"
+    var toggleChatBox = function () {
+        $('.chatBoxHidden').toggleClass('chatBox');
+    };
+
+    //Nav
+    $('.chatBtn').click(function () {
+        toggleChatBox();
+    });
+
+
 });
 
-var conversation = sess.getOrCreateConversation
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-36251023-1']);
+_gaq.push(['_setDomainName', 'jqueryscript.net']);
+_gaq.push(['_trackPageview']);
+
+(function () {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
